7bdb59063a29f7c558358b7fb5e808fede6c7963
AriannaIlvonen <ilvonen@mit.edu>
2024-02-17 19:56:32 +0000

    made new parent folder

--- ros_exercises/install/setup.zsh
+++ ros_exercises/install/setup.zsh
@@ -27,9 +27,6 @@ _colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
 COLCON_CURRENT_PREFIX="/home/sim_ws/install"
 _colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
-# setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
-COLCON_CURRENT_PREFIX="/home/racecar/racecar_ws/src/ros_exercises/install"
-_colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
 
 # source this prefix
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
